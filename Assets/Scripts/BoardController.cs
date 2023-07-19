a4f39338bc1d1e2027e5c5d9e522c8d25ba5d856
ninomiyahayato <a7pseej9@icloud.com>
2023-07-20 00:13:03 +0900

    駒の初期配置設定

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 public class BoardController : MonoBehaviour,IPointerClickHandler
 {
-    [SerializeField, Header("駒")] GameObject _piece;
+    [SerializeField, Header("床")] GameObject _cell;
     [SerializeField] GridLayoutGroup _gridLayoutGroup;
 
     [SerializeField, Header("縦")] int _rows;
@@ -15,6 +15,9 @@ public class BoardController : MonoBehaviour,IPointerClickHandler
     GameObject[,] _bords;
 
     public CellState[,] _cellState;
+    public PieceColor[,] _pieceColor;
+
+    [SerializeField,Header("駒")] GameObject _piece; 
 
     // Start is called before the first frame update
     void Start()
@@ -23,17 +26,23 @@ public class BoardController : MonoBehaviour,IPointerClickHandler
         _gridLayoutGroup.constraintCount = _column;
         _bords = new GameObject[_rows, _column];
         _cellState = new CellState[_rows, _column];
+        _pieceColor = new PieceColor[_rows, _column];
         var parent = _gridLayoutGroup.transform;
         for(int i = 0; i < _rows; i++)
         {
             for(int j = 0; j < _column; j++)
             {
-                var piece = Instantiate(_piece);
-                piece.transform.SetParent(parent);
+                var piece = Instantiate(_cell,transform);
                 _bords[i, j] = piece;
                 _cellState[i, j] = CellState.Empty;
+                _pieceColor[i, j] = PieceColor.None;
+                if(i == 3 && j == 3 || i == 3 && i == 3 && i == 4)
+                {
+
+                }
             }
         }
+        StartSet();
     }
 
     // Update is called once per frame
@@ -45,9 +54,28 @@ public class BoardController : MonoBehaviour,IPointerClickHandler
     {
         var target = eventData.pointerCurrentRaycast.gameObject;
     }
+    public void StartSet()
+    {
+        var num = 3;
+        for (int i = num; i <= num + 1; i++)
+        {
+            var target = _bords[i, num].transform;
+            var target2 = _bords[i, num + 1].transform;
+            var piece = Instantiate(_piece, target);
+            piece.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+            var piece2 = Instantiate(_piece, target2);
+            piece2.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+        }
+    }
 }
-public enum CellState
+public enum CellState //置いてあるかどうか
 {
-    Empty,
-    PiecePlaced,
+    Empty, //空
+    PiecePlaced,//置いてある
+}
+public enum PieceColor //駒のenum
+{
+    None, //何もなし
+    White,//ホワイト
+    Black //ブラック
 }
