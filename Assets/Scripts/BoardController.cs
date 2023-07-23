1bff99f75a39c879b64a2f13c9b8c53afcaa7794
ninomiyahayato <a7pseej9@icloud.com>
2023-07-23 23:48:11 +0900

    駒をとりあえず置けるように次回判定

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class BoardController : MonoBehaviour,IPointerClickHandler
+public class BoardController : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField, Header("床")] GameObject _cell;
     [SerializeField] GridLayoutGroup _gridLayoutGroup;
@@ -14,10 +12,10 @@ public class BoardController : MonoBehaviour,IPointerClickHandler
 
     GameObject[,] _bords;
 
-    public CellState[,] _cellState;
+
     public PieceColor[,] _pieceColor;
 
-    [SerializeField,Header("駒")] GameObject _piece; 
+    [SerializeField, Header("駒")] GameObject _piece;
 
     // Start is called before the first frame update
     void Start()
@@ -25,57 +23,71 @@ public class BoardController : MonoBehaviour,IPointerClickHandler
         _gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
         _gridLayoutGroup.constraintCount = _column;
         _bords = new GameObject[_rows, _column];
-        _cellState = new CellState[_rows, _column];
         _pieceColor = new PieceColor[_rows, _column];
         var parent = _gridLayoutGroup.transform;
-        for(int i = 0; i < _rows; i++)
+        for (int i = 0; i < _rows; i++)
         {
-            for(int j = 0; j < _column; j++)
+            for (int j = 0; j < _column; j++)
             {
-                var piece = Instantiate(_cell,transform);
-                _bords[i, j] = piece;
-                _cellState[i, j] = CellState.Empty;
-                _pieceColor[i, j] = PieceColor.None;
-                if(i == 3 && j == 3 || i == 3 && i == 3 && i == 4)
+                var floor = Instantiate(_cell, transform);
+                _bords[i, j] = floor;
+                _pieceColor[i, j] = PieceColor.Empty;
+                if (i == 3 && j == 3 || i == 4 && j == 4)
                 {
-
+                    var piece = Instantiate(_piece, _bords[i, j].transform);
+                    _pieceColor[i, j] = PieceColor.White;
+                    piece.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+                }
+                else if (i == 3 && j == 4 || i == 4 && j == 3)
+                {
+                    var piece = Instantiate(_piece, _bords[i, j].transform);
+                    _pieceColor[i, j] = PieceColor.Black;
+                    piece.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
                 }
             }
         }
-        StartSet();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnPointerClick(PointerEventData eventData)
     {
         var target = eventData.pointerCurrentRaycast.gameObject;
-    }
-    public void StartSet()
-    {
-        var num = 3;
-        for (int i = num; i <= num + 1; i++)
+        if(ClickCheck(target,out var row,out var column))
         {
-            var target = _bords[i, num].transform;
-            var target2 = _bords[i, num + 1].transform;
-            var piece = Instantiate(_piece, target);
-            piece.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
-            var piece2 = Instantiate(_piece, target2);
-            piece2.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+            if(_pieceColor[row,column] == PieceColor.Empty)
+            {
+                var piece = Instantiate(_piece, _bords[row, column].transform);
+                piece.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+                _pieceColor[row, column] = PieceColor.White;
+            }
         }
     }
-}
-public enum CellState //置いてあるかどうか
-{
-    Empty, //空
-    PiecePlaced,//置いてある
+    public bool ClickCheck(GameObject piece, out int row,out int column)
+    {
+        for(int i = 0; i < _rows; i++)
+        {
+            for(int j = 0; j < _column; j++)
+            {
+                if (piece == _bords[i, j])
+                {
+                    row = i;
+                    column = j;
+                    return true;
+                }
+
+            }
+        }
+        row = 0;column = 0;
+        return false;
+    }
 }
 public enum PieceColor //駒のenum
 {
-    None, //何もなし
+    Empty, //何もなし
     White,//ホワイト
     Black //ブラック
 }
